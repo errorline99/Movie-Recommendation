55e1ed44a46ad7b2e18c843d5184db7b8922e172
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-05-14 04:13:17 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,8 +21,6 @@ export default function RootLayout({ children }) {
           </div>
           <div className="menu-items">
             <li><Link href="/">Home</Link></li>
-            <li><Link href="/components/new">Rated</Link></li>
-            <li><Link href="/search">Browse</Link></li>
           </div>
         </div>
       </div>
